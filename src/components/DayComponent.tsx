338d44e8561a5ce00a5e359cbdee43863ebc2b37
David Feldman <djfeld01@gmail.com>
2023-01-17 18:23:16 -0500

    fixed types

--- src/components/DayComponent.tsx
+++ src/components/DayComponent.tsx
@@ -5,25 +5,27 @@ interface DayComponentProps {
   temp: number,
   day: string
 }
+interface bgColorsType {
 
+}
 function DayComponent(props: DayComponentProps) {
-  const bgColors= {
-    90: 'bg-[#9d0a11] text-gray-300',
-    80: 'bg-[#d26230]',
-    70: 'bg-[#e4c66d]',
-    60: 'bg-[#e0d9bb]',
-    50: 'bg-[#9ca98e]',
-    40: 'bg-[#659fc6]',
-    30: 'bg-[#1868bd] text-gray-200',
-    20: 'bg-[#0a1d39] text-gray-300',
-    10: 'bg-[#735eae] text-gray-300',
+  const bgColors: any= {
+    '90': 'bg-[#9d0a11] text-gray-300',
+    '80': 'bg-[#d26230]',
+    '70': 'bg-[#e4c66d]',
+    '60': 'bg-[#e0d9bb]',
+    '50': 'bg-[#9ca98e]',
+    '40': 'bg-[#659fc6]',
+    '30': 'bg-[#1868bd] text-gray-200',
+    '20': 'bg-[#0a1d39] text-gray-300',
+    '10': 'bg-[#735eae] text-gray-300',
   }
 
   const {temp, day}=props
   
   const formatTemp:number= Math.floor(temp/10) *10;
   
-  const tempBG=bgColors[formatTemp] + ' flex-grow-0'
+  const tempBG:any=bgColors[formatTemp] + ' flex-grow-0'
   console.log(day, temp, formatTemp)
   const tempText='text-temps-800'
 
