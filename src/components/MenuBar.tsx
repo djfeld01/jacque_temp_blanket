0536a31cb8956dcddc64b02578560b6bb05579cd
David Feldman <djfeld01@gmail.com>
2023-01-30 22:03:10 -0500

    changed header

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function MenuBar() {
   return (
-    <div className='sticky top-0 z-50 bg-purple-300 w-full font-extrabold h-10 p-2'>Jacqueline Temperature Blanket</div>
+    <div className='sticky top-0 z-50 bg-black text-white w-full font-extrabold h-10 p-2 shadow-violet-400 shadow-sm'>Jacqueline Temperature Blanket</div>
   )
 }
 
