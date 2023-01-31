3d10fc1c276b45bd813cf500e9cc072fc4dd1244
David Feldman <djfeld01@gmail.com>
2023-01-30 21:56:14 -0500

    adding header fixing mistake

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function MenuBar() {
   return (
-    <div className='sticky top-0 z-50 bg-purple-300 w-full font-extrabold h-10 p-2'>Jacqueline's Temperature Blanket</div>
+    <div className='sticky top-0 z-50 bg-purple-300 w-full font-extrabold h-10 p-2'>Jacqueline Temperature Blanket</div>
   )
 }
 
